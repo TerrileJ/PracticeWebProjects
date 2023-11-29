f9e12cdf8152cb578f40236f2d9d9a8d86ef2ced
Terrile John <terrilejohn101@gmail.com>
2023-11-28 17:21:37 -0800

    Added more operations buttons (0,.,clear,etc)

--- Calculator/src/Main.java
+++ Calculator/src/Main.java
@@ -8,6 +8,7 @@ public class Main {
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(new Dimension(250,300));
+        frame.setResizable(false);
 
         // result label
         JLabel display = new JLabel();
@@ -19,8 +20,11 @@ public class Main {
 
         // grid of buttons
         JPanel buttons = new JPanel();
-        buttons.setLayout(new GridLayout(4,4));
+        buttons.setLayout(new GridLayout(5,4));
         buttons.setPreferredSize(new Dimension(200,200));
+
+        // numbers
+        JButton zero = new JButton("0");
         JButton one = new JButton("1");
         JButton two = new JButton("2");
         JButton three = new JButton("3");
@@ -30,6 +34,17 @@ public class Main {
         JButton seven = new JButton("7");
         JButton eight = new JButton("8");
         JButton nine = new JButton("9");
+        JButton decimalPoint = new JButton(".");  // used as placeholders for more aesthetic UI
+
+        // operations
+        JButton plus = new JButton("+");
+        JButton minus = new JButton("-");
+        JButton multiply = new JButton("*");
+        JButton divide = new JButton("/");
+        JButton delete = new JButton("Del");
+        JButton clear = new JButton("Clr");
+        JButton negative = new JButton("(-)");
+        JButton equals = new JButton("=");
 
         one.addActionListener(e -> display.setText(display.getText() + "1"));
         two.addActionListener(e -> display.setText(display.getText() + "2"));
@@ -40,12 +55,6 @@ public class Main {
         seven.addActionListener(e -> display.setText(display.getText() + "7"));
         eight.addActionListener(e -> display.setText(display.getText() + "8"));
         nine.addActionListener(e -> display.setText(display.getText() + "9"));
-
-        JButton plus = new JButton("+");
-        JButton minus = new JButton("-");
-        JButton multiply = new JButton("*");
-        JButton divide = new JButton("/");
-        JButton delete = new JButton("Del");
 
         buttons.add(one);
         buttons.add(two);
@@ -59,8 +68,13 @@ public class Main {
         buttons.add(eight);
         buttons.add(nine);
         buttons.add(multiply);
-        buttons.add(delete);
+        buttons.add(zero);
+        buttons.add(decimalPoint);
+        buttons.add(equals);
         buttons.add(divide);
+        buttons.add(negative);
+        buttons.add(delete);
+        buttons.add(clear);
 
         // add to frame
         frame.add(display);
